015cf58e99335289b13c2b54bfd9c5380c35a965
Mark Coleman <m.coleman@uglyduckling.nl>
2014-03-11 14:36:35 +0000

    Added IP address to payload of self-healing message.

--- agent/docker/serf/restart-proc.sh
+++ agent/docker/serf/restart-proc.sh
@@ -2,16 +2,19 @@
 
 FILENAME_BREAK=/tmp/restart/break.tmp
 FILENAME_OVERLOAD=/tmp/restart/load.tmp
+IP_ADDRESS=`hostname -i`
 while [ true ]
 do
    sleep 1
    if [ -f $FILENAME_BREAK ]; then
-      echo "Restarting service and removing $FILENAME_BREAK"
+      echo "Restartingi service and removing $FILENAME_BREAK"
+      serf event FIXING $IP_ADDRESS
+      sleep 2
       rm $FILENAME_BREAK
-      serf event FIXED
+      serf event FIXED $IP_ADDRESS
    elif [ -f $FILENAME_OVERLOAD ]; then
       echo "Restarting service and removing $FILENAME_OVERLOAD"
       rm $FILENAME_OVERLOAD
-      serf event OVERLOADED 
+      serf event OVERLOADED
    fi
 done
--- agent/docker/serf/serf-event-handler.sh
+++ agent/docker/serf/serf-event-handler.sh
@@ -1,8 +1,9 @@
  #!/bin/bash
+read AGENT_IP
 if [ "${SERF_USER_EVENT}" = "OVERLOADED" ]; then
-        echo "create new container for $SERF_SELF_NAME" >> /tmp/test.txt
+        echo "create new container for $SERF_SELF_NAME $AGENT_IP" >> /tmp/test.txt
         /usr/bin/docker run -d -name node3 -link node1:serf uglyduckling.nl/serf
 elif [ "${SERF_USER_EVENT}" = "FIXED" ]; then
-        echo "fixed agent $SERF_SELF_NAME" >> /tmp/test.txt
+        echo "fixed agent $SERF_SELF_NAME $AGENT_IP" >> /tmp/test.txt
 fi
 echo "${SERF_USER_EVENT}"
