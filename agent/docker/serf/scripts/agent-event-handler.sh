888e139ce0216938365c2602f2fae36164017ef9
Mark Coleman <m.coleman@uglyduckling.nl>
2014-03-13 10:15:30 +0000

    Fixed issue where the first agent to join would not be given an event handler. Added clearer logging to the agent event handler; IP always included, only filter user events, standard logging for all messages.

--- agent/docker/serf/scripts/agent-event-handler.sh
+++ agent/docker/serf/scripts/agent-event-handler.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 MEMORY_FILE=/tmp/memory.tmp
-LOG_FILE=/tmp/logging/`hostname -i`.log
-echo 1 >> $LOG_FILE
-read PAYLOAD
-echo 2 >> $LOG_FILE
-if [ "${SERF_USER_EVENT}" = "TEST_SET_MEMORY" ]; then
-  echo 3 >> $LOG_FILE
-  serf event TEST_SET_EVENT_RCVD
-  echo "Received TEST_SET_MEMORY event on $AGENT_IP with payload of $PAYLOAD" >> $LOG_FILE
-  echo $PAYLOAD > $MEMORY_FILE
-  serf event $PAYLOAD
+IP_ADDRESS=`hostname -i`
+LOG_FILE=/tmp/logging/$IP_ADDRESS.log
+
+if [ "${SERF_EVENT}" = "user" ]; then # Only process user events in here
+  read PAYLOAD
+  echo $IP_ADDRESS - Received user event: ${SERF_USER_EVENT} with payload of $PAYLOAD >> $LOG_FILE
+  if [ "${SERF_USER_EVENT}" = "TEST_SET_MEMORY" ]; then
+    echo $IP_ADDRESS - Processing user event: ${SERF_USER_EVENT} with payload of $PAYLOAD >> $LOG_FILE
+    echo $PAYLOAD > $MEMORY_FILE
+    echo $IP_ADDRESS - Processed user event: ${SERF_USER_EVENT} with payload of $PAYLOAD >> $LOG_FILE
+  fi
 fi
 echo "${SERF_USER_EVENT}"
--- agent/docker/serf/scripts/serf.sh
+++ agent/docker/serf/scripts/serf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 # taken forn Anton Lindström
 # https://github.com/antonlindstrom/docker-serf
 set -e
@@ -9,15 +9,20 @@ LOG_FILE=/tmp/logging/$IP_ADDRESS.log
 LINE=`cat /proc/1/cgroup | tail -n 1`
 echo ${LINE: -64} >> $LOG_FILE
 
+JOIN_STRING=""
+
 supervisord
 
-# Check if serf-port is set, else just start the node
+# Check if serf-port is set and if so define the join string
 if [ -n "$SERF_PORT_7946_TCP_ADDR" ]; then
-  echo "Found: $SERF_PORT_7946_TCP_ADDR"
-  serf agent -join $SERF_PORT_7946_TCP_ADDR -event-handler=`pwd`/agent-event-handler.sh -role=functional_agent
-else
-  echo "No links, running agent."
-  serf agent -role=serf
+  echo "Found: $SERF_PORT_7946_TCP_ADDR" >> $LOG_FILE
+  echo "Before assignment" >> $LOG_FILE
+  JOIN_STRING="-join $SERF_PORT_7946_TCP_ADDR"
+  echo "After assignment" >> $LOG_FILE
+  echo $JOIN_STRING
 fi
+
+echo Joining $IP_ADDRESS with event handler >> $LOG_FILE
+serf agent $JOIN_STRING -event-handler=`pwd`/agent-event-handler.sh -role=functional_agent
 
 
