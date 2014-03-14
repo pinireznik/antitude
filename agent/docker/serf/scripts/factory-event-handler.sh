b6488049b3b0e028cf7ae72ce04cf9985907de49
preznik <preznik@piniubuntults.(none)>
2014-03-14 10:01:02 +0100

    add REMOVENODE event

--- agent/docker/serf/scripts/factory-event-handler.sh
+++ agent/docker/serf/scripts/factory-event-handler.sh
@@ -7,14 +7,16 @@ read PAYLOAD
 echo "Received user event ${SERF_USER_EVENT} with payload $PAYLOAD" >> $LOG_FILE
 TEST_FILE=./test/test.txt
 
-read PAYLOAD
-
 if [ "${SERF_USER_EVENT}" = "NEWNODE" ]; then
   HOSTNAME=`hostname`
   IP_ADDRESS=`./serf members | grep $HOSTNAME | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
   echo $IP_ADDRESS > agentstartup/factoryip
   echo "Creating new container for $PAYLOAD" >> $LOG_FILE
   /usr/bin/docker run -d -v `pwd`/logging:/tmp/logging -v `pwd`/agentstartup:/tmp/factoryip uglyduckling.nl/serf
+elif [ "${SERF_USER_EVENT}" = "REMOVENODE" ]; then
+  echo "Removing container with ID = $PAYLOAD" >> $LOG_FILE
+  /usr/bin/docker kill $PAYLOAD
+  /usr/bin/docker rm $PAYLOAD
 elif [ "${SERF_USER_EVENT}" = "FIXED" ]; then
         echo "fixed agent $PAYLOAD" >> $LOG_FILE
 	# writing to the test file
