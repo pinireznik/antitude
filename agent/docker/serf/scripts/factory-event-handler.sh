a2a9227cb779023316f80a7200120f45f48a85cc
Mark Coleman <m.coleman@uglyduckling.nl>
2014-03-14 10:05:36 +0000

    Factory now uses -e to pass it's IP to the containers instead of mounting a file via ./agentstartup.

--- agent/docker/serf/scripts/factory-event-handler.sh
+++ agent/docker/serf/scripts/factory-event-handler.sh
@@ -7,20 +7,16 @@ read PAYLOAD
 echo "Received user event ${SERF_USER_EVENT} with payload $PAYLOAD" >> $LOG_FILE
 TEST_FILE=./test/test.txt
 
-read PAYLOAD
-
 if [ "${SERF_USER_EVENT}" = "NEWNODE" ]; then
   HOSTNAME=`hostname`
   IP_ADDRESS=`./serf members | grep $HOSTNAME | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
-  echo $IP_ADDRESS > agentstartup/factoryip
-  echo "Creating new container for $PAYLOAD" >> $LOG_FILE
-  /usr/bin/docker run -d -v `pwd`/logging:/tmp/logging -v `pwd`/agentstartup:/tmp/factoryip uglyduckling.nl/serf
+  /usr/bin/docker run -e "FACTORY_IPADDRESS=$IP_ADDRESS" -d -v `pwd`/logging:/tmp/logging -v `pwd`/agentstartup:/tmp/factoryip uglyduckling.nl/serf
 elif [ "${SERF_USER_EVENT}" = "FIXED" ]; then
-        echo "fixed agent $PAYLOAD" >> $LOG_FILE
-	# writing to the test file
-	echo "$PAYLOAD ${SERF_USER_EVENT}" >> $TEST_FILE
+  echo "fixed agent $PAYLOAD" >> $LOG_FILE
+  # writing to the test file
+  echo "$PAYLOAD ${SERF_USER_EVENT}" >> $TEST_FILE
 elif [ "${SERF_USER_EVENT}" = "MEMORY" ]; then
-        echo "memory use on agent $PAYLOAD" >> $LOG_FILE
+  echo "memory use on agent $PAYLOAD" >> $LOG_FILE
 fi
 echo "`date '+%F %T'` ${SERF_USER_LTIME} ${SERF_EVENT} ${SERF_USER_EVENT} PAYLOAD=${PAYLOAD}" >> $LOG_FILE
 
--- agent/docker/serf/scripts/serf.sh
+++ agent/docker/serf/scripts/serf.sh
@@ -8,23 +8,18 @@ IP_ADDRESS=`hostname -i`
 LOG_FILE=/tmp/logging/$IP_ADDRESS.log
 LINE=`cat /proc/1/cgroup | tail -n 1`
 echo ${LINE: -64} >> $LOG_FILE
-FACTORYIP_FILE=/tmp/factoryip/factoryip
 
 JOIN_STRING=""
 
 supervisord
 
 # Check if serf-port is set and if so define the join string
-if [ -n "$SERF_PORT_7946_TCP_ADDR" ]; then
-  echo "Found: $SERF_PORT_7946_TCP_ADDR" >> $LOG_FILE
-  JOIN_STRING="-join $SERF_PORT_7946_TCP_ADDR"
-  echo $JOIN_STRING
-elif [ -f "$FACTORYIP_FILE" ]; then
-  echo "Found $FACTORYIP_FILE" >> $LOG_FILE
-  JOIN_STRING="-join `cat $FACTORYIP_FILE`"
+if [ -n $FACTORY_IPADDRESS ]; then
+  echo "Found: $FACTORY_IPADDRESS" >> $LOG_FILE
+  JOIN_STRING="-join $FACTORY_IPADDRESS"
 fi
 
-echo Joining $IP_ADDRESS with event handler >> $LOG_FILE
+echo serf agent $JOIN_STRING -event-handler=`pwd`/agent-event-handler.sh -role=functional_agent >> $LOG_FILE
 serf agent $JOIN_STRING -event-handler=`pwd`/agent-event-handler.sh -role=functional_agent >> $LOG_FILE
 
 
