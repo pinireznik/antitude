f25e4e33e6507b2f719849e055de5bad54543208
Adrian Mouat <a.mouat@uglyduckling.nl>
2014-03-19 14:39:13 +0000

    Removed call to deleted test.

--- agents/serf/test.sh
+++ agents/serf/test.sh
@@ -3,7 +3,6 @@
 #java -cp "./MitosisTest/lib/hamcrest-core-1.3.jar:./MitosisTest/lib/junit.jar:./MitosisTest/bin" org.junit.runner.JUnitCore SpeakToCommandLine
 echo RUNNING UNIT TESTS
 docker run -i -t uglyduckling.nl/serf ./TestAgentEventHandler.py
-docker run -i -t uglyduckling.nl/serf ./TestMockOsEnviron.py
 
 echo RUNNING SYSTEM TESTS
 echo NO SYSTEM TESTS YET
