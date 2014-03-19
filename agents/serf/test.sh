d33479b151f69a3fb524c3025bac38218e625c1c
Pini Reznik <p.reznik@uglyduckling.nl>
2014-03-19 17:11:58 +0100

    fix the test docker command

--- agents/serf/test.sh
+++ agents/serf/test.sh
@@ -2,7 +2,7 @@
 
 # java -cp "./MitosisTest/lib/hamcrest-core-1.3.jar:./MitosisTest/lib/junit.jar:./MitosisTest/bin" org.junit.runner.JUnitCore SpeakToCommandLine
 echo RUNNING UNIT TESTS
-docker run -i -t uglyduckling.nl/serf ./TestAgentEventHandler.py
+docker run uglyduckling.nl/serf ./TestAgentEventHandler.py
 RETURN_CODE=$?
 
 echo RUNNING SYSTEM TESTS
