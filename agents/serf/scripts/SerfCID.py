a6db7ab00e004c9130b1d248098d26bd82800714
Adrian Mouat <a.mouat@uglyduckling.nl>
2014-03-19 13:59:51 +0000

    Added logging to SerfCID.

--- agents/serf/scripts/SerfCID.py
+++ agents/serf/scripts/SerfCID.py
@@ -1,3 +1,6 @@
+import logging
+
+
 CGROUP_FILE = "/proc/1/cgroup"
 CID_LENGTH = 64
 
@@ -9,5 +12,6 @@ def getCID():
             CID = CID_FULL_LINE[-CID_LENGTH:]  # Strip off the last CID_LENGTH (Normally 64) characters and return them
             return CID
     except Exception as e:
-        print e
+        logger = logging.getLogger(__name__)
+        logger.error("Failed to retrieve CID: %s" % e)
         return None
