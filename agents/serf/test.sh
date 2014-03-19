a18b35d91f6abfda8a2c590a99ea95abb25c3137
Pini Reznik <p.reznik@uglyduckling.nl>
2014-03-19 16:30:23 +0100

    test CI scheduling

--- agents/serf/test.sh
+++ agents/serf/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#java -cp "./MitosisTest/lib/hamcrest-core-1.3.jar:./MitosisTest/lib/junit.jar:./MitosisTest/bin" org.junit.runner.JUnitCore SpeakToCommandLine
+# java -cp "./MitosisTest/lib/hamcrest-core-1.3.jar:./MitosisTest/lib/junit.jar:./MitosisTest/bin" org.junit.runner.JUnitCore SpeakToCommandLine
 echo RUNNING UNIT TESTS
 docker run -i -t uglyduckling.nl/serf ./TestAgentEventHandler.py
 RETURN_CODE=$?
