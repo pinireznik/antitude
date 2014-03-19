b39c2e2b5dc3107bc8737445cdafdf5793d7c60b
Pini Reznik <p.reznik@uglyduckling.nl>
2014-03-19 16:22:10 +0100

    return correct status in test.sh

--- agents/serf/test.sh
+++ agents/serf/test.sh
@@ -3,6 +3,9 @@
 #java -cp "./MitosisTest/lib/hamcrest-core-1.3.jar:./MitosisTest/lib/junit.jar:./MitosisTest/bin" org.junit.runner.JUnitCore SpeakToCommandLine
 echo RUNNING UNIT TESTS
 docker run -i -t uglyduckling.nl/serf ./TestAgentEventHandler.py
+RETURN_CODE=$?
 
 echo RUNNING SYSTEM TESTS
 echo NO SYSTEM TESTS YET
+
+exit $RETURN_CODE
