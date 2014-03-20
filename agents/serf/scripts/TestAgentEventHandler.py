58d0db76bca29dc44f42f888b93c5456855de71e
Adrian Mouat <adrian.mouat@gmail.com>
2014-03-20 12:16:15 +0000

    Simplified import and changed to pep8 standard.
    Now for viz stuff...

--- agents/serf/scripts/TestAgentEventHandler.py
+++ agents/serf/scripts/TestAgentEventHandler.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import AgentEventHandler
+from AgentEventHandler import AgentEventHandler
 import unittest
 import os
 import logging
@@ -9,7 +9,8 @@ from testfixtures import LogCapture
 class TestAgentEventHandler(unittest.TestCase):
 
     CID = "ce6437097f5683f0b9fdf01295450753ba600e33562cd4675500fdb03bfd1ff0"
-    WRONG_CID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    WRONG_CID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\
+                XXX"
     PAYLOAD_WITH_TARGET = "TARGET=" + CID + " MEMORY_LEVEL=80"
     PAYLOAD_NO_TARGET = "MEMORY_LEVEL=80"
     TARGET_ARGUMENT_KEY = "TARGET"
@@ -23,52 +24,70 @@ class TestAgentEventHandler(unittest.TestCase):
         pass
 
     def testGetPayload(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET)
-        self.assertEqual(self.PAYLOAD_WITH_TARGET, agentEventHandler.getPayload())
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET)
+        self.assertEqual(
+            self.PAYLOAD_WITH_TARGET, agentEventHandler.getPayload())
 
     def testGetArgumentPairs(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET)
-        self.assertEqual(self.TARGET_ARGUMENT_PAIR, agentEventHandler.getArgumentPair(self.TARGET_ARGUMENT_KEY))
-        self.assertEqual(self.MEMORY_LEVEL_ARGUMENT_PAIR, agentEventHandler.getArgumentPair(self.MEMORY_LEVEL_ARGUMENT_KEY))
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET)
+        self.assertEqual(
+            self.TARGET_ARGUMENT_PAIR,
+            agentEventHandler.getArgumentPair(self.TARGET_ARGUMENT_KEY))
+        self.assertEqual(
+            self.MEMORY_LEVEL_ARGUMENT_PAIR,
+            agentEventHandler.getArgumentPair(self.MEMORY_LEVEL_ARGUMENT_KEY))
 
     def testGetArgumentValues(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET)
-        self.assertEqual(self.TARGET_ARGUMENT_VALUE, agentEventHandler.getArgumentValue(self.TARGET_ARGUMENT_PAIR))
-        self.assertEqual(self.MEMORY_LEVEL_ARGUMENT_VALUE, agentEventHandler.getArgumentValue(self.MEMORY_LEVEL_ARGUMENT_PAIR))
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET)
+        self.assertEqual(
+            self.TARGET_ARGUMENT_VALUE,
+            agentEventHandler.getArgumentValue(self.TARGET_ARGUMENT_PAIR))
+        self.assertEqual(
+            self.MEMORY_LEVEL_ARGUMENT_VALUE,
+            agentEventHandler.getArgumentValue(
+                self.MEMORY_LEVEL_ARGUMENT_PAIR))
 
     def testGetCIDDefault(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET)
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET)
         self.assertEqual("", agentEventHandler.getCID())
 
     def testGetCID(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET, self.CID)
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET, self.CID)
         self.assertEqual(self.CID, agentEventHandler.getCID())
 
     def testCorrectTargetWithCorrectCID(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET, self.CID)
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET, self.CID)
         self.assertEqual(True, agentEventHandler.correctTarget())
 
     def testCorrectTargetWithInCorrectCID(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_WITH_TARGET, self.WRONG_CID)
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_WITH_TARGET, self.WRONG_CID)
         self.assertEqual(False, agentEventHandler.correctTarget())
 
     def testCorrectTargetWithNoTargetInPayload(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_NO_TARGET, self.CID)
+        agentEventHandler = AgentEventHandler(self.PAYLOAD_NO_TARGET, self.CID)
         self.assertEqual(True, agentEventHandler.correctTarget())
 
     def testEnvVarWithOsEnviron(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(self.PAYLOAD_NO_TARGET, self.CID, os.environ)
+        agentEventHandler = AgentEventHandler(
+            self.PAYLOAD_NO_TARGET, self.CID, os.environ)
         self.assertEqual(None, agentEventHandler.getEnvVar("TEST!"))
 
     def testEnvVarWithMockOsEnviron(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(
+        agentEventHandler = AgentEventHandler(
             self.PAYLOAD_NO_TARGET,
             self.CID,
             {"SERF_EVENT": "user", "SERF_USER_EVENT": "TEST_SET_MEMORY"})
         self.assertEqual("user", agentEventHandler.getEnvVar("SERF_EVENT"))
 
     def testSerfEventIs(self):
-        agentEventHandler = AgentEventHandler.AgentEventHandler(
+        agentEventHandler = AgentEventHandler(
             self.PAYLOAD_NO_TARGET,
             self.CID,
             {"SERF_EVENT": "user", "SERF_USER_EVENT": "TEST_SET_MEMORY"})
@@ -80,26 +99,28 @@ class TestAgentEventHandler(unittest.TestCase):
             logger.info("Called memory handler")
 
         with LogCapture() as l:
-            agentEventHandler = AgentEventHandler.AgentEventHandler(
+            agentEventHandler = AgentEventHandler(
                 payload=self.PAYLOAD_WITH_TARGET,
                 CID=self.CID,
-                envVars={"SERF_EVENT": "user", "SERF_USER_EVENT": "TEST_SET_MEMORY"},
+                envVars={"SERF_EVENT": "user",
+                         "SERF_USER_EVENT": "TEST_SET_MEMORY"},
                 handlers={"TEST_SET_MEMORY": testMemoryHandler})
             agentEventHandler.handleShit()
-            self.checkLogMessages(l, 
-              "Processing user event: TEST_SET_MEMORY",
-              "Called memory handler",
-              "Processed")
+            self.checkLogMessages(l,
+                                  "Processing user event: TEST_SET_MEMORY",
+                                  "Called memory handler",
+                                  "Processed")
 
     def checkLogMessages(self, log, *messages):
-      log_str = str(log)
-      ind = 0
-      for m in messages:
-        try:
-          new_ind = log_str.index(m, ind) 
-          ind = new_ind + len(m)
-        except ValueError, e:
-          self.fail('Message "%s" did not appear in: \n %s' % (m, log_str[ind:-1]))
-  
+        log_str = str(log)
+        ind = 0
+        for m in messages:
+            try:
+                new_ind = log_str.index(m, ind)
+                ind = new_ind + len(m)
+            except ValueError:
+                self.fail('Message "%s" did not appear in: \n %s'
+                          % (m, log_str[ind:-1]))
+
 if __name__ == '__main__':
     unittest.main()
