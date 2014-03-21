074800c39eb2c2198aea256c484d46a1d668186a
Adrian Mouat <adrian.mouat@gmail.com>
2014-03-21 14:35:44 +0000

    UI container now has it's own agent for listening to events.

---
+++ agents/ui/ui-event-handler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+LOG_FILE=logging/ui.log
+read PAYLOAD
+echo "`date '+%F %T'` Received user event ${SERF_USER_EVENT} with payload $PAYLOAD" >> $LOG_FILE
+
+echo "`date '+%F %T'` ${SERF_USER_LTIME} ${SERF_EVENT} ${SERF_USER_EVENT} PAYLOAD=${PAYLOAD}" >> $LOG_FILE
+
+if [[ -z "${PAYLOAD}" ]]; then
+	curl -i -X PUT -H "Content-Type:text/plain"  http://localhost:5000/send/${SERF_USER_EVENT}
+else 
+	curl -i -X PUT -H "Content-Type:text/plain" -d "${PAYLOAD}" http://localhost:5000/send/${SERF_USER_EVENT}
+fi
