b09b887ba5e52c4ea55ed6ef06f691d79ff43327
Adrian Mouat <adrian.mouat@gmail.com>
2014-03-21 16:36:07 +0000

    Added curl.
    Oops.

--- agents/ui/ui-event-handler.sh
+++ agents/ui/ui-event-handler.sh
@@ -6,7 +6,7 @@ echo "`date '+%F %T'` Received user event ${SERF_USER_EVENT} with payload $PAYLO
 echo "`date '+%F %T'` ${SERF_USER_LTIME} ${SERF_EVENT} ${SERF_USER_EVENT} PAYLOAD=${PAYLOAD}" >> $LOG_FILE
 
 if [[ -z "${PAYLOAD}" ]]; then
-	curl -i -X PUT -H "Content-Type:text/plain"  http://localhost:5000/send/${SERF_USER_EVENT}
+	curl -i -X PUT -H "Content-Type:text/plain"  http://127.0.0.1:5000/send/${SERF_USER_EVENT}
 else 
-	curl -i -X PUT -H "Content-Type:text/plain" -d "${PAYLOAD}" http://localhost:5000/send/${SERF_USER_EVENT}
+	curl -i -X PUT -H "Content-Type:text/plain" -d "${PAYLOAD}" http://127.0.0.1:5000/send/${SERF_USER_EVENT}
 fi
