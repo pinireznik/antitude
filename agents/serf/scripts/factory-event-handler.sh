dbfb1e4f27faa6735b1683dc017ef1746266e28f
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-03-26 12:01:16 +0100

    The parent tag is now set if a node is instantiated by another node

--- agents/serf/scripts/factory-event-handler.sh
+++ agents/serf/scripts/factory-event-handler.sh
@@ -15,10 +15,14 @@ fi
 
 if [ "${SERF_USER_EVENT}" = "NEWNODE" ] || [ "${SERF_USER_EVENT}" = "NEWUINODE" ]; then
   HOSTNAME=`hostname`
-  ROLE=`echo $PAYLOAD | cut -d "=" -f 2`
+  for params in `echo $PAYLOAD | tr " " "\n"`
+  do
+    printf -v `echo $params | cut -d "=" -f 1` `echo $params | cut -d "=" -f 2`
+    echo DEBUG: $parent >> $LOG_FILE
+  done
   IP_ADDRESS=`./serf members | grep $HOSTNAME | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
   echo "`date '+%F %T'` Creating node with role $ROLE and attaching to factory at $IP_ADDRESS" >> $LOG_FILE
-  CID=$(/usr/bin/docker run -e "AGENT_ROLE=$ROLE" -e "FACTORY_IPADDRESS=$IP_ADDRESS" -e "EVENT_HANDLER=${EVENT_HANDLER}" -d -v `pwd`/logging:/tmp/logging -v `pwd`/simulation:/tmp/simulation -v `pwd`/configs:/tmp/configs ${DOCKER_PORT_EXPOSE} ${DOCKER_IMAGE} bash /serf.sh)
+  CID=$(/usr/bin/docker run -e "AGENT_PARENT=$parent" -e "AGENT_ROLE=$role" -e "FACTORY_IPADDRESS=$IP_ADDRESS" -e "EVENT_HANDLER=${EVENT_HANDLER}" -d -v `pwd`/logging:/tmp/logging -v `pwd`/simulation:/tmp/simulation -v `pwd`/configs:/tmp/configs ${DOCKER_PORT_EXPOSE} ${DOCKER_IMAGE} bash /serf.sh)
   NEWNODE_IP=`/usr/bin/docker inspect $CID | grep IPAddress | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
   echo "`date '+%F %T'` Created new node with CID: $CID and public IP: $NEWNODE_IP" >> $LOG_FILE
   ./serf event NODECREATED $CID $NEWNODE_IP
--- agents/serf/scripts/serf.sh
+++ agents/serf/scripts/serf.sh
@@ -32,8 +32,16 @@ else
   echo "Found role: $AGENT_ROLE" >> $LOG_FILE
 fi
 
-
-serf agent $JOIN_STRING -event-handler=`pwd`$EVENT_HANDLER -role=${AGENT_ROLE} >> $LOG_FILE &
+serf agent $JOIN_STRING -event-handler=`pwd`$EVENT_HANDLER  >> $LOG_FILE &
+sleep 2 # Cant set the tags if the agent is not connected
+serf tags -set role=${AGENT_ROLE}
+echo DEBUG: $AGENT_PARENT >> $LOG_FILE
+if [ -z $AGENT_PARENT ]; then
+  echo "Parent variable not present" >> $LOG_FILE
+else
+  echo "Setting parent to $AGENT_PARENT" >> $LOG_FILE
+  serf tags -set parent=$AGENT_PARENT
+fi
 
 if [ -e /tmp/configs/$AGENT_ROLE.cfg ]; then
   echo "Config file found for role $AGENT_ROLE" >> $LOG_FILE
@@ -41,7 +49,7 @@ if [ -e /tmp/configs/$AGENT_ROLE.cfg ]; then
   for dep in $deps
   do
     echo "Requesting dependency: $dep" >> $LOG_FILE
-    serf event -coalesce=false NEWNODE role=$dep
+    serf event -coalesce=false NEWNODE "role=$dep parent=$AGENT_ROLE"
   done
 fi
 
