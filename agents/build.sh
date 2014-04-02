3ca33bd5bfc6857a5b8942bbb4c35947badefaf3
Adrian Mouat <a.mouat@uglyduckling.nl>
2014-04-02 08:50:16 +0000

    Trying to add python-webapp as submodule.

--- agents/build.sh
+++ agents/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [ "$1" != "serf-base" ] && [ "$1" != "skynet" ] && [ "$1" != "ui" ] && [ "$1" != "resman" ] && [ "$1" != "database" ] && [ "$1" != "webserver" ] && [ "$1" != "loadbalancer" ]
-
+if [ "$1" != "serf-base" ] && [ "$1" != "skynet" ] && [ "$1" != "ui" ] && [ "$1" != "resman" ] && [ "$1" != "database" ] && [ "$1" != "webserver" ] && [ "$1" != "loadbalancer" ] && [ "$1" != "python_webapp" ]
   then
     echo "Which image do you want to build?" 
-    echo "build <serf-base/skynet/ui/resman/database/webserver/loadbalancer>"
+    echo "build <serf-base/skynet/ui/resman/database/webserver/loadbalancer/python_webapp>"
     exit 1;
 fi
 echo "Copying Dockerfile.${1} to  Dockerfile"
