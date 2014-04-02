3048112dd7e00ef51fb279c20e0459fdd4a18662
Pini Reznik <p.reznik@uglyduckling.nl>
2014-04-02 10:28:14 +0200

    move deletelogs file and add containers build to the restart

---
+++ agents/deletelogs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -fv logging/*
+rm -rf shared/simulation/*
+rm -rf shared/logging/*
--- agents/restart.sh
+++ agents/restart.sh
@@ -3,6 +3,9 @@
 kill -15 `pidof serf`
 ./build.sh skynet
 ./build.sh ui
+./build.sh loadbalancer
+./build.sh webserver
+./build.sh database 
 # Do not rebuild base everytime 
 #./build.sh serf-base
 ./start-factory.sh > /dev/null 2>&1 &
