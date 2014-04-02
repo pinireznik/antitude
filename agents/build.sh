dce7420ddd3dcfa2f2bd048b5e6c6b405541e91e
Adrian Mouat <a.mouat@uglyduckling.nl>
2014-04-02 09:13:25 +0000

    Python webapp fixes.

--- agents/build.sh
+++ agents/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$1" != "serf-base" ] && [ "$1" != "skynet" ] && [ "$1" != "ui" ] && [ "$1" != "resman" ] && [ "$1" != "database" ] && [ "$1" != "webserver" ] && [ "$1" != "loadbalancer" ] && [ "$1" != "python_webapp" ]
+if [ "$1" != "serf-base" ] && [ "$1" != "skynet" ] && [ "$1" != "ui" ] && [ "$1" != "resman" ] && [ "$1" != "database" ] && [ "$1" != "webserver" ] && [ "$1" != "loadbalancer" ] && [ "$1" != "python-webapp" ]
   then
     echo "Which image do you want to build?" 
-    echo "build <serf-base/skynet/ui/resman/database/webserver/loadbalancer/python_webapp>"
+    echo "build <serf-base/skynet/ui/resman/database/webserver/loadbalancer/python-webapp>"
     exit 1;
 fi
 echo "Copying Dockerfile.${1} to  Dockerfile"
